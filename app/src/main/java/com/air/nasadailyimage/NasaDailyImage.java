64a4e7963563a1c502d9f82b6600a3dd7fc3aa5f
AIR <air.cse.pbn@gmail.com>
2015-07-23 02:30:29 +0600

    Initial Commit

---
+++ app/src/main/java/com/air/nasadailyimage/NasaDailyImage.java
@@ -0,0 +1,16 @@
+package com.air.nasadailyimage;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class NasaDailyImage extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nasa_daily_image);
+    }
+}
